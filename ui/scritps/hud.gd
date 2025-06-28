7ae38c3d02d95ebc8c9b8b0ae9a69818875675f6
mauro.oviedo <mauro.oviedo2@gmail.com>
2025-06-28 20:52:55 -0300

    adjust HUD

--- ui/scritps/hud.gd
+++ ui/scritps/hud.gd
@@ -1,8 +1,23 @@
 extends Control
 
+@export var cloud_speed: int = 22
+
 func _ready() -> void:
 	LevelVariables.show_info_layer = false
 	LevelVariables.show_pause_menu = false
+	
+	$margin_container_buttons/box_container_buttons/button_new_game/btn_new_game_bone_left.visible = false
+	$margin_container_buttons/box_container_buttons/button_new_game/btn_new_game_bone_right.visible = false
+	$margin_container_buttons/box_container_buttons/button_options/btn_options_right.visible = false
+	$margin_container_buttons/box_container_buttons/button_options/btn_options_left.visible = false
+	$margin_container_buttons/box_container_buttons/button_exit/btn_exit_right.visible = false
+	$margin_container_buttons/box_container_buttons/button_exit/btn_exit_left.visible = false
+	
+func _physics_process(delta: float) -> void:
+	_moveClouds(delta)
+	
+func _moveClouds(delta: float) -> void:
+	$Parallax2D/ParallaxLayer.motion_offset.x -= cloud_speed * delta
 
 func _on_button_new_game_pressed() -> void:
 	LevelVariables.current_level = 1
@@ -12,3 +27,28 @@ func _on_button_options_pressed() -> void:
 
 func _on_button_exit_pressed() -> void:
 	get_tree().quit()
+
+func _on_button_new_game_mouse_entered() -> void:
+	$margin_container_buttons/box_container_buttons/button_new_game/btn_new_game_bone_left.visible = true
+	$margin_container_buttons/box_container_buttons/button_new_game/btn_new_game_bone_right.visible = true
+
+
+func _on_button_new_game_mouse_exited() -> void:
+	$margin_container_buttons/box_container_buttons/button_new_game/btn_new_game_bone_left.visible = false
+	$margin_container_buttons/box_container_buttons/button_new_game/btn_new_game_bone_right.visible = false
+
+func _on_button_options_mouse_entered() -> void:
+	$margin_container_buttons/box_container_buttons/button_options/btn_options_right.visible = true
+	$margin_container_buttons/box_container_buttons/button_options/btn_options_left.visible = true
+
+func _on_button_options_mouse_exited() -> void:
+	$margin_container_buttons/box_container_buttons/button_options/btn_options_right.visible = false
+	$margin_container_buttons/box_container_buttons/button_options/btn_options_left.visible = false
+
+func _on_button_exit_mouse_entered() -> void:
+	$margin_container_buttons/box_container_buttons/button_exit/btn_exit_right.visible = true
+	$margin_container_buttons/box_container_buttons/button_exit/btn_exit_left.visible = true
+
+func _on_button_exit_mouse_exited() -> void:
+	$margin_container_buttons/box_container_buttons/button_exit/btn_exit_right.visible = false
+	$margin_container_buttons/box_container_buttons/button_exit/btn_exit_left.visible = false
